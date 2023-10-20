406ea24bf8464b813b28ea9fde4b98f8d3cf00e4
redgodkill <tf00185077@gmail.com>
2023-10-20 10:06:52 +0800

    node

---
+++ components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>im footer</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Welcome to the homepages</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+</template>
